c5dca6c3de7d7bef4881c972f3ce2b276f4cc711
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-30 18:54:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,33 @@
+def using_push(array, string)
+  array = []
+  string = "violet"
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array = ["@new_neighborhood", 3, 4, 5]
+  string = "Staten Island"
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+
+def pop_with_args(array)
+  array = ["Dog1", "Dog2", "Chihuahua", "Shiba Inu"]
+  p array.pop(2)
+end
+
+def using_shift(array)
+  array = ["Lagos", "2", "3", "4"]
+  new_array = array.shift
+  
+end
+
+def shift_with_args(array)
+  array = ["Blue Bell Creameries", "Ben & Jerry's", "3"]
+  new_array = array.shift(2)
+end
+  
